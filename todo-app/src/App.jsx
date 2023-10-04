fcfe7391d749b6e27c9d836a03151f38f3fbc22c
lisab1993 <brown.lisa999@gmail.com>
2023-10-04 16:07:05 -0700

    deployment attempt... again

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,10 +1,11 @@
+
 import './index.css'
 
 function App() {
 
   return (
     <>
-    <h1 className='text-rose-600 text-4xl font-bold'>Heather and Ruby</h1>
+    <h1 className='font-bold'>Happy Halloween!!! </h1>
     </>
   )
 }
